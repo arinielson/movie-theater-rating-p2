a808f7f9729d931206f75cbeeadbc2ec10cd19c8
Kenna Fackrell <kennac28@gmail.com>
2022-12-31 11:51:53 -0700

    added theater data with name and address, and added user info with name and email

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,1 +1,34 @@
+const { DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+
+
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+        },
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [20],
+        },
+    },
+    }
+);
