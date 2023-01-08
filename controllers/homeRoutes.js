92832cbed19fbd3256e7248301110dc6e981470b
Kenna Fackrell <kennac28@gmail.com>
2023-01-07 18:23:09 -0700

    added .env file and added info to homeRoutes.js page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,23 @@ const router = require('express').Router();
 const { Theatre, User, Address, Review } = require('../models');
 
 router.get('/', async (req, res) => {
-    res.render('homepage', {});
+    Theatre.findAll({
+        where: {
+            id: 1
+        }
+    })
+       .then(data => res.json(data))
+       .then(data => {
+            res.render('homepage',data)
+ 
+       })
+    // res.render('homepage', 
+    //     Theatre.findAll({
+    //         where: {
+    //             id: 1
+    //         }
+    //     })
+    // );
 
     
 
