e9a8d47a1ce9818527c8c9d461b7ec00e54fbee4
Kenna Fackrell <kennac28@gmail.com>
2023-01-09 12:19:51 -0700

    added userdata info to reviewdata.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const routes = require('./controllers');
 const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const reviewData = JSON.parse(fs.readFileSync('./reviewData.json'));
 
 const app = express();
 const PORT = process.env.PORT || 3001;
